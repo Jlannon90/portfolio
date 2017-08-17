91682b41ced20493e851a40f39d57469d96aed20
James Lannon <jamesmlannon1990@live.com>
2017-08-17 00:46:43 -0700

    finish media queries for tablet size, add show hide functionality for work section and info for a new project

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-//business logic stuff for a particular module
-var ExampleModule = function(args) {
-  this.args = args; //to be replaced with constructor arguments
-};
-
-ExampleModule.prototype.examplePrototype = function() {
-  return `this is an example prototype method`;
-};
-
-exports.exampleModule = ExampleModule;
-
 $(function () {
 	$('.hamburger-menu').on('click', function() {
 		$('.bar').toggleClass('animate');
     $('.menu').fadeToggle(800);
 	})
+  $('.right-arrow').on('click', function() {
+    $('.proj-title, .proj-img, .proj-desc, .left-arrow, .right-arrow').hide();
+		$('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').show();
+	})
+  $('.left-arrow-2').on('click', function() {
+    $('.proj-title, .proj-img, .proj-desc, .left-arrow, .right-arrow').show();
+		$('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').hide();
+	})
+
 })();
