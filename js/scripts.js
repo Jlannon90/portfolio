d54c1fc2a12e75cc19f4a08ad76861c5d5131264
James Lannon <jamesmlannon1990@live.com>
2017-08-22 09:48:15 -0700

    add max width to body, add slow scrolling in js for hamburger menu links

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
 $(function () {
+  //hamburger menu
 	$('.hamburger-menu').on('click', function() {
 		$('.bar').toggleClass('animate');
     $('.menu').fadeToggle(800);
-	})
+	});
+  //custom carousel
+
   $('.right-arrow').on('click', function() {
     $('.proj-title, .proj-img, .proj-desc, .left-arrow, .right-arrow').hide();
 		$('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').show();
-	})
+	});
   $('.left-arrow-2').on('click', function() {
     $('.proj-title, .proj-img, .proj-desc, .left-arrow, .right-arrow').show();
 		$('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').hide();
-	})
+	});
+
+  //scrolling
+  $("a").on('click', function(event) {
+
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1800, function(){
+      window.location.hash = hash;
+    });
+  }
+});
 
 })();
