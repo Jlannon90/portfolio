422b82b178b6884a0a2aaafb567b85f4b537d394
James Lannon <jamesmlannon1990@live.com>
2017-08-23 13:57:29 -0700

    create new icons for process section, adjust media queries for work section for images to fix mobile size issues, add 4 projects to work section, add js to wrap up carousel functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,51 @@ $(function () {
 		$('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').hide();
 	});
 
+  $('.right-arrow-2').on('click', function() {
+    $('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').hide();
+		$('.proj-title-3, .proj-img-3, .proj-desc-3, .left-arrow-3, .right-arrow-3').show();
+	});
+  $('.left-arrow-3').on('click', function() {
+    $('.proj-title-2, .proj-img-2, .proj-desc-2, .left-arrow-2, .right-arrow-2').show();
+		$('.proj-title-3, .proj-img-3, .proj-desc-3, .left-arrow-3, .right-arrow-3').hide();
+	});
+
+  $('.right-arrow-3').on('click', function() {
+    $('.proj-title-3, .proj-img-3, .proj-desc-3, .left-arrow-3, .right-arrow-3').hide();
+		$('.proj-title-4, .proj-img-4, .proj-desc-4, .left-arrow-4, .right-arrow-4').show();
+	});
+  $('.left-arrow-4').on('click', function() {
+    $('.proj-title-3, .proj-img-3, .proj-desc-3, .left-arrow-3, .right-arrow-3').show();
+		$('.proj-title-4, .proj-img-4, .proj-desc-4, .left-arrow-4, .right-arrow-4').hide();
+	});
+
+  $('.right-arrow-4').on('click', function() {
+    $('.proj-title-4, .proj-img-4, .proj-desc-4, .left-arrow-4, .right-arrow-4').hide();
+		$('.proj-title-5, .proj-img-5, .proj-desc-5, .left-arrow-5, .right-arrow-5').show();
+	});
+  $('.left-arrow-5').on('click', function() {
+    $('.proj-title-4, .proj-img-4, .proj-desc-4, .left-arrow-4, .right-arrow-4').show();
+		$('.proj-title-5, .proj-img-5, .proj-desc-5, .left-arrow-5, .right-arrow-5').hide();
+	});
+
+  $('.right-arrow-5').on('click', function() {
+    $('.proj-title-5, .proj-img-5, .proj-desc-5, .left-arrow-5, .right-arrow-5').hide();
+		$('.proj-title-6, .proj-img-6, .proj-desc-6, .left-arrow-6, .right-arrow-6').show();
+	});
+  $('.left-arrow-6').on('click', function() {
+    $('.proj-title-5, .proj-img-5, .proj-desc-5, .left-arrow-5, .right-arrow-5').show();
+		$('.proj-title-6, .proj-img-6, .proj-desc-6, .left-arrow-6, .right-arrow-6').hide();
+	});
+
+  $('.right-arrow-6').on('click', function() {
+    $('.proj-title-6, .proj-img-6, .proj-desc-6, .left-arrow-6, .right-arrow-6').hide();
+    $('.proj-title, .proj-img, .proj-desc, .left-arrow, .right-arrow').show();
+  });
+  $('.left-arrow').on('click', function() {
+    $('.proj-title-6, .proj-img-6, .proj-desc-6, .left-arrow-6, .right-arrow-6').show();
+    $('.proj-title, .proj-img, .proj-desc, .left-arrow, .right-arrow').hide();
+  });
+
   //scrolling
   $("a").on('click', function(event) {
 
